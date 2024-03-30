12c3a6f0de61014107b65b9720b60d8eb83b5236
Kostya <kostyabogdanov338@gmail.com>
2024-03-30 11:17:11 +0300

    java initial commit

---
+++ bookler-app/bookler-web-app/src/main/java/com/bookler/booklerwebapp/BooklerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.bookler.booklerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklerWebAppApplication.class, args);
+	}
+
+}
