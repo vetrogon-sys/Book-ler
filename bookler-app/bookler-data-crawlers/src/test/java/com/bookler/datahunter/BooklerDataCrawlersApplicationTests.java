71be017d9771950464d3fef372a9711199b6fc90
Kostya <kostyabogdanov338@gmail.com>
2024-03-30 11:13:20 +0300

    java initial commit

---
+++ bookler-app/bookler-data-crawlers/src/test/java/com/bookler/datahunter/BooklerDataCrawlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookler.datahunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklerDataCrawlersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ bookler-app/bookler-web-app/src/test/java/com/bookler/booklerwebapp/BooklerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookler.booklerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
